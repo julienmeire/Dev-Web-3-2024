b191aae71b192697b2795860e4535b535226daa5
julienmeire <71373002+julienmeire@users.noreply.github.com>
2024-06-11 18:28:40 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const cors = require('cors');
 app.use(bodyParser.json());
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/frontend/dist/posts')));
+app.use(express.static(path.join(__dirname, '../frontend/dist/posts')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
