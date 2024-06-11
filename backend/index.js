662f841d968563c6ff3bcf8f45bc62c220fda8fe
julienmeire <71373002+julienmeire@users.noreply.github.com>
2024-06-11 14:51:27 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,8 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+app.use(express.static('frontend'));
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
